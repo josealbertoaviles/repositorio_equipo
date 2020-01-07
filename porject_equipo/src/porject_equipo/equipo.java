847d91be6d435db3489420a8b2c9b18877216bb5
jose.aviles <jose.aviles@1josaviles6501X.babel-si.local>
2020-01-07 11:05:09 +0100

    primer commit Jose Alberto

---
+++ porject_equipo/src/porject_equipo/equipo.java
@@ -0,0 +1,10 @@
+package porject_equipo;
+
+public class equipo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("trabajo en equipo");
+	}
+
+}
